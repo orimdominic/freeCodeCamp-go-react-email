58eb6f5bf30daa9876811aec0f864b6b50186e0e
Orim Dominic <orimdominic@gmail.com>
2026-04-17 18:47:23 +0100

    ft: include built html output

--- mailer/_emails/src/welcome.tsx
+++ mailer/_emails/src/welcome.tsx
@@ -12,8 +12,18 @@ import {
   Text,
 } from "react-email";
 
-const WelcomeEmail = () => {
-  const previewText = `Welcome to ((.Company)), ((.Username))!`;
+interface WelcomeEmailProps {
+  username?: string;
+  company?: string;
+  gophers?: string[];
+}
+
+const WelcomeEmail = ({
+  username = "Nicole",
+  company = "GoWorld",
+  gophers = ["Tinky Winky", "Dipsy", "Laa-Laa", "Po"],
+}: WelcomeEmailProps) => {
+  const previewText = `Welcome to ${company}, ${username}!`;
 
   return (
     <Html>
@@ -27,40 +37,39 @@ const WelcomeEmail = () => {
                 src={`https://storage.googleapis.com/gopherizeme.appspot.com/gophers/69428e5ec867c34bb4a49d5a063fdbc2a6633aed.png`}
                 width="80"
                 height="80"
-                alt="Gopher"
+                alt="Logo"
                 className="my-0 mx-auto"
               />
             </Section>
             <Heading className="text-2xl font-normal text-center p-0 my-8 mx-0">
-              Welcome to <strong>((.Company))</strong>, ((.Username))!
+              Welcome to <strong>{company}</strong>, {username}!
             </Heading>
-            <Text className="text-start text-sm">Hello ((.Username)),</Text>
-            <Text className="text-start text-sm leading-relaxed">
-              We're excited to have you onboard at <strong>((.Company))</strong>
-              . We hope you enjoy your journey with us. If you have any
-              questions or need assistance, feel free to reach out to any of the
-              following Gophers:
+            <Text className="text-start text-base">Hello {username},</Text>
+            <Text className="text-start text-base leading-relaxed">
+              We're excited to have you onboard at <strong>{company}</strong>.
+              We hope you enjoy your journey with us. If you have any questions
+              or need assistance, feel free to reach out to any of the following
+              gophers:
             </Text>
-            <div className="text-start text-sm leading-relaxed">
+            <div className="text-start text-base leading-relaxed">
               <ul className="pl-3">
-                ((range .Gophers))
-                <li>((.))</li>
-                ((end))
+                {gophers.map((gopher) => (
+                  <li>{gopher}</li>
+                ))}
               </ul>
             </div>
             <Section className="text-center mt-[32px] mb-[32px]">
               <Button
-                className="py-2.5 px-5 bg-white rounded-md border text-black text-sm font-semibold no-underline text-center"
+                className="py-2.5 px-5 bg-white rounded-md text-base font-semibold no-underline text-center bg-black text-white"
                 href={`https://go.dev`}
               >
                 Get Started
               </Button>
             </Section>
-
-            <Text className="text-start text-sm">
+            <Text className="text-start text-base text-white">
               Cheers,
               <br />
-              The ((.Company)), Team
+              The {company} Team
             </Text>
           </Container>
         </Body>
